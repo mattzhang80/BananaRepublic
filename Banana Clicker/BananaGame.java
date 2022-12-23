6e7b5ebae7da2d5b787d9ee43dfd960275330d01
mattzhang80 <115035711+mattzhang80@users.noreply.github.com>
2022-12-22 23:35:12 -0500

    Add files via upload

---
+++ Banana Clicker/BananaGame.java
@@ -0,0 +1,27 @@
+/* Creates a new BananaRepublic.java which basically runs the game. In order to
+start and play the game, run this file. Follows website reccomendation to create
+game in one .java file and run it in another. Similar to ImageClassifier way.
+ */
+public class BananaGame {
+    public static void main(String[] args) {
+        new BananaRepublic();
+        // To test feature #1, all we can really do is run this file and see if
+        // it has three panels, one for minion icons, one with a banana clicker
+        // and one for the minion shop
+
+        // Feature #2:  for (String key : minionStorage)
+        //                     System.out.println(minionStorage.get(key));
+        // on line 262.
+
+        // Feature #3: again, just click on clicking at different speeds and
+        // see cps and banana count. Could Output but already on screen, redundant.
+        // Buy minions and see the rate of bps and bananaCounter goes up right.
+
+        // Feature #4
+        // to test randomess, use client and see if prints generally random quotes
+        // to see array accuracy , for (int i = 0; i < quotes.length; i++)
+        //             System.out.println(quotes[i]);
+        // at line 79
+
+    }
+}
